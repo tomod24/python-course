84969ac94a1e1ee6b794034a9cad49ed20590e0e
David Paul Thompson <thomo_d24@hotmail.co.uk>
2019-01-01 15:42:57 +0000

    byotest.py

--- evens.py
+++ evens.py
@@ -1,52 +1,33 @@
-"""
-Solution to the challenge in the `Test Before` unit found in the `Test Driven
-Development with Python lesson`
-NOTE: The solution found in this file is one of the many potential solutions
-that can be used to achieve the end result expected by the challenge in the
-lesson.
-"""
-
-
-def even_number_of_evens(numbers):
-    """
-    Returns the number of even numbers contained in a list of numbers.
-    `numbers` should be a list containing numbers
+def number_of_evens(numbers):
+    return 0
     
-    Returns either True or False based on a number of criteria.
-        - if `numbers` is empty, return `False`
-        - if the number of even numbers is odd, return `False`
-        - if the number of even numbers is 0, return `False`
-        - if the number of even numbers is even, return `True`
-    """
+def test_are_equal(actual, expected):
+    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
 
-    # Check to see if the list is empty
-    if numbers == []:
-        return False
-    else:
-        # Set a `number_of_evens` variable that will be incremented each time
-        # an even number is found
-        evens = 0
-        
-    # Iterate of over each item and if it's an even number, increment the
-    # `evens` variable
-    for number in numbers:
-        if number % 2 == 0:
-            evens += 1
+def test_not_equal(a,b):
+    assert a != b, "Did not expect {0} but got{1}".format(a,b)
     
-    if evens == 0:
-        return False
-    else:
-        return evens % 2 == 0
+    def test_is_in(collection, item):
+        assert item in collection, "{0} does not contain [1}".format(collection, item)
+    
+test_are_equal(number_of_evens([1,2,3,4,5]), 2)
 
-# Our set of test cases
-assert even_number_of_evens([]) == False, "No numbers"
-assert even_number_of_evens([2]) == False, "One even number"
-assert even_number_of_evens([2, 4]) == True, "Two even numbers"
-assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
-assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three even"
-assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple numbers, four even"
-assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
 
-# If all the test cases pass, print some successful info to the console to let
-# the developer know
-print("All tests passed!")
+
+#testing
+#def is_even(number):
+    #return number % 2 == 0
+
+#def even_number_of_evens(numbers):
+
+    #evens = sum([1 for n in numbers if is_even(n)])
+    #return False if evens == 0 else is_even(evens)
+
+    
+#assert even_number_of_evens([]) == False, "No Numbers"
+
+#assert even_number_of_evens([2, 4]) == True, "Two even numbers"
+#assert even_number_of_evens([2]) == False, "One even number"
+#assert even_number_of_evens([1,3, 9]) == False, "Three Numbers"
+
+#print("All tests passed!")
